f2c67cff552873e7cb82a9883295f11c36df662a
kaulashish <kaulashish@protonmail.com>
2021-01-04 13:44:04 +0530

    Added welcome_message fxn

--- functions.py
+++ functions.py
@@ -1,7 +1,10 @@
 import models
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
 from sqlalchemy import create_engine
 from getpass import getpass
+import numpy as np
+from tabulate import tabulate
 
 engine = create_engine(
     "mysql+mysqldb://root:root@localhost:3306/ticketsystem", echo=False
@@ -86,10 +89,85 @@ def password_checking(password, username):
             break
 
 
+def welcome_message():
+    print("TICKET BOOKING SYSTEM".center(40, "*"))
+    print("\n")
+    status = session.execute(select([models.User.username, models.User.usertype]))
+    for row in status:
+        if row[0] == username and row[1] == True:
+            print("1. Init seats")
+            print("2. Show seats")
+            print("3. Add movies")
+            print("4. Show statistics")
+            print("0. Quit")
+
+            prompt = int(input("Enter choice: "))
+            if prompt == 1:
+                init_seats()
+
+            if prompt == 2:
+                show_seats()
+
+        elif row[0] == username and row[1] == False:
+            print("1. Show seats")
+            print("2. Buy a ticket")
+            print("3. Show booked tickets user info")
+            print("0. Exit")
+
+            prompt = int(input("Enter option: "))
+            if prompt == 1:
+                show_seats()
+
+
+def init_seats():
+    global seat_rows, seat_columns, seats
+    seat_rows = int(input("Enter the number of rows: "))
+    seat_columns = int(input("Enter the number of columns: "))
+    seats = np.full((seat_rows, seat_columns), "S")
+    print("Seats Initialized successfully!")
+    prompt = str(input("Show seats? (y/n): "))
+    if prompt.lower() == "y":
+        show_seats()
+    elif prompt.lower() == "n":
+        welcome_message()
+
+
+def show_seats():
+    print("\nCinema: ")
+    headers = [x for x in range(1, seat_columns + 1)]
+    table = tabulate(seats, headers, showindex=True, tablefmt="pretty")
+    print(table)
+
+
+def add_movies():
+    pass
+
+
+def statistics():
+    pass
+
+
+def buy_ticket():
+    pass
+
+
+def booked_ticket_info():
+    pass
+
+
+# x = session.query(models.User).first()
+# result = session.execute(select([models.User.password_hash]))
+# print(result)
+# for row in result:
+#     print(row)
+
+init_seats()
+
 session.close()
 
 
-# add_user("ashishadmin")
+# add_user("testuser2")
 # usr = search_user("ashishadmin")
+# print(session.execute(select([usr.password_hash])))
 # print(usr[0].password_hash)
-print(password_checking("testing123", "ashish01"))
+# print(password_checking("testing123", "ashish01"))
